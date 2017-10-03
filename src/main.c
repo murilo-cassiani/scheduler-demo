3e057c06a16ae0711ff9a0a75f394bceda034836
murilo-cassiani <murilo.cassiani@gmail.com>
2017-10-03 08:23:34 -0300

    Funcionou com todos os testes

--- src/main.c
+++ src/main.c
@@ -6,8 +6,22 @@ int main() {
 
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
 
-  for (int i=0; i<n; i++) {
+  for (int i=1; i<=n; i++) {
     /* Imprime resultado da n-esima iteracao do scheduler */
+  	if(!(i%a[0]))
+  		printf("0");
+  	if(!(i%a[1]))
+  		printf("1");
+  	if(!(i%a[2]))
+  		printf("2");
+  	if(!(i%a[3]))
+  		printf("3");
+  	if(!(i%a[4]))
+  		printf("4");
+  	if((!(i%a[0])) || (!(i%a[1])) || (!(i%a[2])) || (!(i%a[3])) || (!(i%a[4])))
+  		printf("\n");
+  	else
+  		printf("-\n");
   }
 
   return 0;
